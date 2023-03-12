84341e54d04b260b58806fc6ce2549223a60ca58
Adam Pangelinan <49807160+bubbapang@users.noreply.github.com>
2023-03-11 19:07:33 -0800

    Revert "Express setup"

--- backend/app.js
+++ backend/app.js
@@ -1,63 +1,20 @@
-const debug = require('debug')
-const express = require("express");
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const cors = require('cors');
-const { isProduction } = require('./config/keys');
-const csurf = require('csurf');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const usersRouter = require('./routes/api/users'); // update the import file path
-const tweetsRouter = require("./routes/api/tweets");
-const csrfRouter = require('./routes/api/csrf');
+var app = express();
 
-const app = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(logger('dev')); // log request components (URL/method) to terminal
-app.use(express.json()); // parse JSON request body
-app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
-app.use(cookieParser()); // parse cookies as an object on req.cookies
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-if (!isProduction) {
-  // Enable CORS only in development because React will be on the React
-  // development server (http://localhost:3000). (In production, the Express 
-  // server will serve the React files statically.)
-    app.use(cors());
-}
-
-// ...
-app.use(
-    csurf({
-    cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true
-    }
-    })
-);
-
-app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.statusCode = 404;
-    next(err);
-});
-
-const serverErrorLogger = debug('backend:error');
-
-app.use((err, req, res, next) => {
-    serverErrorLogger(err);
-    const statusCode = err.statusCode || 500;
-    res.status(statusCode);
-    res.json({
-    message: err.message,
-    statusCode,
-    errors: err.errors
-})
-});
-
-
-// Attach Express routers
-app.use('/api/users', usersRouter); // update the path
-app.use('/api/tweets', tweetsRouter);
-app.use('/api/csrf', csrfRouter);
 module.exports = app;
---
+++ backend/routes/users.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('Respond with a user resource HAHAHAHHAHHAHAHAHAHHAHAHHA');
+  
+});
+
+module.exports = router;
