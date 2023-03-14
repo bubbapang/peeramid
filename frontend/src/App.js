83509e1df9eb24d66ad02d8ee837346b06b08406
Adam Pangelinan <adamtpangelinan@gmail.com>
2023-03-14 11:49:43 -0700

    Add basic pyramid for day rating page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,32 @@
-// import react shit 
+// import react shit
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 // import session shit
 import { getCurrentUser } from './store/session';
 
 // import components
-import { AuthRoute } from './components/Routes/Routes';
-// import Welcome from './components/Welcome';
-
-//import Rating from './components/Rating';
+import Welcome from './components/Welcome';
 import Rating from './components/Rating';
 import HomePage from './components/HomePage';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(getCurrentUser()).then(() => setLoaded(true));
   }, [dispatch]);
+
   return loaded && (
     <>
       <Switch>
-        {/* <Rating /> */}
-        <HomePage />
+        <Route exact path="/" component={Welcome} />
+        <Route exact path="/rating" component={Rating} />
+        <Route exact path="/home" component={HomePage} />
+
         {/* <AuthRoute exact path="/" component={Welcome} /> */}
         {/* <ProtectedRoute exact path="/tweets" component={Tweets} />
         <ProtectedRoute exact path="/profile" component={Profile} />
