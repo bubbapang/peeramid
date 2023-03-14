d38c3313679e85720e13b851670cea9341e4f49e
andrehanna8 <andrehanna3@gmail.com>
2023-03-14 10:14:00 -0700

    added updates to homepage

---
+++ frontend/src/components/HomePage/index.js
@@ -0,0 +1,12 @@
+import NavBar from '../NavBar';
+import './HomePage.css';
+
+export default function HomePage() {
+    return (
+        <div className="home-page-container">
+            <NavBar />
+            <h1>HomePage</h1>
+        </div>
+
+    )
+}
