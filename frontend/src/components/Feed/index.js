f7231d00d51e85bca7e8b87188ab79f8f8563069
andrehanna8 <andrehanna3@gmail.com>
2023-03-14 16:56:07 -0700

    pushing so adam can fix file tree

--- frontend/src/components/Feed/index.js
+++ frontend/src/components/Feed/index.js
@@ -3,10 +3,30 @@ import './Feed.css';
 export default function Feed() {
     // const ratings = useSelector(state => state.ratings); to add later
 
+    const posts = [
+        //each post will have a user object as well as the 8 ratings from maslows hierarchy of needs
+        {
+            user: {
+                id: 1,
+                username: "user1",
+                profilePic: "https://i.imgur.com/3XQX5Z0.jpg"
+            },
+            ratings: {
+                physiological: 5,
+                safety: 4,
+                love: 3,
+                esteem: 2,
+                cognitition: 1,
+                aesthetics: 5,
+                actualization: 4,
+                transcendence: 3,
+            }
+        }
+    ]
     return (
         <div className="feed-container">
-            {/* {ratings.map(rating => 
-                <FeedIndexItem post={rating} />
+            {/* {posts.map(post => 
+                <FeedIndexItem post={post} />
             )} */}
             <h1>Feed</h1>
         </div>
