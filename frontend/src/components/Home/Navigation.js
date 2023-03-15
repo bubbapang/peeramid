37be616d5acf21913cb193a4fef8f8c4264387f4
Adam Pangelinan <adamtpangelinan@gmail.com>
2023-03-14 19:39:38 -0700

    Add home and nav components

--- frontend/src/components/Home/Navigation.js
+++ frontend/src/components/Home/Navigation.js
@@ -1,26 +1,27 @@
-import { useDispatch } from "react-redux";
-import { logout } from "../../store/session";
-import './Navigation.css'
+// import { useDispatch } from "react-redux";
+// import { logout } from "../../store/session";
+import React from 'react';
+import './Navigation.css';
 
-export default function Navigation(props) {
-    const dispatch = useDispatch();
+export default function Navigation({ onButtonClick }) {
+    // const dispatch = useDispatch();
 
     const handleButtonClick = (buttonName) => {
-        props.onButtonClick(buttonName);
-    }
+        onButtonClick(buttonName);
+    };
 
-    const logUserOut = () => {
-        dispatch(logout())
-        window.location.href = '/';
-    }
+    // const logUserOut = () => {
+    //     dispatch(logout());
+    //     window.location.href = '/';
+    // };
 
     return (
         <div className="nav-bar-container">
-            <button onClick={() => handleButtonClick('Profile')}>Profile</button>
+            <button className="profile-icon" onClick={() => handleButtonClick('Profile')}><img src="https://via.placeholder.com/30" alt="Profile" /></button>
+            <button className="today" onClick={() => handleButtonClick('Rating')}>Today</button>
+            <div className="search-container"><input type="text" placeholder="Search..." className="search-input" /></div>
             <button onClick={() => handleButtonClick('Feed')}>Feed</button>
             <button onClick={() => handleButtonClick('Suggestions')}>Suggestions</button>
-            <button onClick={() => handleButtonClick('Rating')}>Rate your day</button>
-            <button onClick={logUserOut}>Log Out</button>
         </div>
-    )
+    );
 }
