bb2901520d4b266aef8e468cc4d4b30074481f95
JasmineKobata <jasmineorjasnice@gmail.com>
2023-03-15 23:56:57 -0700

    added thunk action files

---
+++ frontend/src/store/likes.js
@@ -0,0 +1,54 @@
+import jwtFetch from "./jwt";
+
+export const RECEIVE_LIKES = `likes/RECEIVE_LIKES`;
+export const RECEIVE_LIKE = `likes/RECEIVE_LIKE`;
+export const REMOVE_LIKE = `likes/REMOVE_LIKE`;
+
+export const receiveLikes = (userId) => {
+    return {
+        type: RECEIVE_LIKES,
+        userId
+    }
+}
+
+export const receiveLike = (suggestionId) => {
+    return {
+        type: RECEIVE_LIKE,
+        suggestionId
+    }
+}
+
+export const removeLike = (suggestionId) => {
+    return {
+        type: REMOVE_LIKE,
+        suggestionId
+    }
+}
+
+export const getLikes = (userId) => (store) => {
+    return store.users[userId] ? store.users[userId].likes : []
+}
+
+export const fetchLikes = (userId) => async (dispatch) => {
+    const response = await jwtFetch(`/api/users/${userId}/likes`);
+
+    if (response.ok) {
+        dispatch(receiveLikes(userId));
+    }
+}
+
+export const createLike = (suggestionId) => async (dispatch) => {
+    const response = await jwtFetch(`/api/suggestions/${suggestionId}/likes`);
+
+    if (response.ok) {
+        dispatch(receiveLike(suggestionId));
+    }
+}
+
+export const deleteLike = (suggestionId) => async (dispatch) => {
+    const response = await jwtFetch(`/api/suggestions/${suggestionId}/likes`);
+
+    if (response.ok) {
+        dispatch(removeLike(suggestionId));
+    }
+}
