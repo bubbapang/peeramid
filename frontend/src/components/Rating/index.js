764b645369dcb52fdb9dab622d65045f308ebdc6
Adam Pangelinan <adamtpangelinan@gmail.com>
2023-03-17 08:59:54 -0700

    Fix form

--- frontend/src/components/Rating/index.js
+++ frontend/src/components/Rating/index.js
@@ -51,12 +51,10 @@ export default function Rating() {
     const widthsOfNeeds = [500, 600, 700, 800, 900, 1000, 1100, 1200];
 
     const handleInfoClick = () => {
-        const infoModal = document.getElementById("infoModal");
-        if (infoModal.style.display === "none" || infoModal.style.display === "") {
-        infoModal.style.display = "block";
-        } else {
-        infoModal.style.display = "none";
-        }
+        // const infoModal = document.getElementById("infoModal");
+        // const needs = document.getElementsById("needs");
+        // infoModal.style.display = "none";
+        // needs.style.display = "none";
     };
 
     const handleSubmit = (e) => {
@@ -80,28 +78,35 @@ export default function Rating() {
     };
 
     return (
-        <div className='needs-wrapper'>
-        <div className='info-stuff'>
-            <button className="highlight">Highlight</button>
-            <div className="modal" id="infoModal">
-            <div className="modal-content">
-            <h3>Maslow's extended hierarchy of needs builds upon his initial five-tier model, which includes physiological, safety, love/belonging, esteem, and self-actualization needs. In the extended version, Maslow added three more levels: cognitive, aesthetic, and transcendence. Cognitive needs relate to knowledge and understanding, while aesthetic needs encompass beauty and order. Transcendence needs, the highest level, involve helping others achieve self-actualization. The extended hierarchy offers a more comprehensive view of human motivation, emphasizing the pursuit of knowledge, appreciation of beauty, and the desire to help others grow.</h3>
-        </div>
-        </div>
-            <button className="info" id="infoButton" onClick={handleInfoClick}>Info</button>
-            {/* Add the Lowlight button */}
-            <button className="lowlight">Lowlight</button>
+        <div className='today'>
+            <div className='left-side'>
+                <button className="highlight">Highlight</button>
+                <button className="info" id="infoButton" onClick={handleInfoClick}>Info</button>
+                {/* Add the Lowlight button */}
+                <button className="lowlight">Lowlight</button>
             </div>
-            <div className='needs'>
-            {namesOfNeeds.map((name, idx) => {
-                const color = colorsOfNeeds[idx];
-                const width = widthsOfNeeds[idx];
-                return (
-                <Need key={idx} name={name} color={color} width={width} onRatingChange={handleRating} />
-                )
-            }, [])}
-        </div>
-        <button className='submit' onClick={handleSubmit}>Submit</button>
+            <div className="modal">
+                <div className="modal-content">
+                    <h3>Maslow's extended hierarchy of needs builds upon his initial five-tier model, 
+                        which includes physiological, safety, love/belonging, esteem, and self-actualization needs. 
+                        In the extended version, Maslow added three more levels: cognitive, aesthetic, and transcendence. 
+                        Cognitive needs relate to knowledge and understanding, while aesthetic needs encompass beauty and order. 
+                        Transcendence needs, the highest level, involve helping others achieve self-actualization. 
+                        The extended hierarchy offers a more comprehensive view of human motivation, emphasizing the pursuit of knowledge, 
+                        appreciation of beauty, and the desire to help others grow.
+                    </h3>
+                </div>
+            </div>
+            <div className='needs' id='needs'>
+                    {namesOfNeeds.map((name, idx) => {
+                        const color = colorsOfNeeds[idx];
+                        const width = widthsOfNeeds[idx];
+                        return (
+                        <Need key={idx} name={name} color={color} width={width} onRatingChange={handleRating} />
+                        )
+                    }, [])}
+                </div>
+            <button className='submit' onClick={handleSubmit}>Submit</button>
         </div>
     );
 }
