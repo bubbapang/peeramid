8dcb6906ee9910ed102522cf412b6f264971a298
Adam Pangelinan <adamtpangelinan@gmail.com>
2023-03-23 17:53:07 -0700

    Fix user auth errors

--- frontend/src/components/Feed/FeedItem.js
+++ frontend/src/components/Feed/FeedItem.js
@@ -1,66 +1,81 @@
-import { useRef, useEffect, useState } from 'react';
-import Chart from 'chart.js/auto';
-import './FeedItem.css';
-import { useDispatch } from 'react-redux';
-import { createSuggestion } from '../../store/suggestions';
+import { useRef, useEffect, useState } from "react";
+import Chart from "chart.js/auto";
+import "./FeedItem.css";
+import { useDispatch } from "react-redux";
+import { createSuggestion } from "../../store/suggestions";
 
-function FormDrawer({ onClose, visible, closing, rating, clickedLabel, onSuccess }) {
+function FormDrawer({
+  onClose,
+  visible,
+  closing,
+  rating,
+  clickedLabel,
+  onSuccess,
+}) {
   const dispatch = useDispatch();
 
   const submitSuggestionForm = async (e) => {
     e.preventDefault();
     const newSuggestion = {
-      body: document.getElementById('suggestion_body').value,
+      body: document.getElementById("suggestion_body").value,
       dayRating: rating._id,
       categoryTag: clickedLabel,
-    }
+    };
 
     await dispatch(createSuggestion(newSuggestion, rating._id));
-    const suggestionCreatedEvent = new CustomEvent('suggestionCreated');
+    const suggestionCreatedEvent = new CustomEvent("suggestionCreated");
     window.dispatchEvent(suggestionCreatedEvent);
 
     onSuccess();
 
     onClose();
-  }
-
+  };
 
   return (
-    <div className={`form-drawer${visible ? ' visible' : ''}${closing ? ' closing' : ''}`}>
-  
-
+    <div
+      className={`form-drawer${visible ? " visible" : ""}${
+        closing ? " closing" : ""
+      }`}
+    >
       <button className="close-button" onClick={onClose}>
         Close
       </button>
-
       <h2>{rating.user.username}</h2>
       {/* Your form content goes here */}
       <div className="form-drawer-content">
-          {/* input text box and label to send a suggestion */}
-          <div className="form-drawer-input">
-              <label htmlFor="suggestion">Suggestion</label>
-              <button> You are submitting a suggestion based on the user's: <span id="label">{clickedLabel} </span> </button>
-              <br></br>
-              <br></br>
-              <input type="text" id="suggestion_body" name="suggestion" placeholder="Enter a suggestion" />
-              <button className="form-drawer-button" onClick={submitSuggestionForm}>Send</button>
-          </div>
-      </div>  
-
+        {/* input text box and label to send a suggestion */}
+        <div className="form-drawer-input">
+          <label htmlFor="suggestion">Suggestion</label>
+          <button>
+            {" "}
+            You are submitting a suggestion based on the user's:{" "}
+            <span id="label">{clickedLabel} </span>{" "}
+          </button>
+          <br></br>
+          <br></br>
+          <input
+            type="text"
+            id="suggestion_body"
+            name="suggestion"
+            placeholder="Enter a suggestion"
+          />
+          <button className="form-drawer-button" onClick={submitSuggestionForm}>
+            Send
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
 
-
-  export default function FeedItem({ rating, idx }) {
-    const [showFormDrawer, setShowFormDrawer] = useState(true);
-
-    const [formDrawerClosing, setFormDrawerClosing] = useState(false);
-    const chartRef = useRef(null);
-    const [formDrawerVisible, setFormDrawerVisible] = useState(false);
-    const [clickedLabel, setClickedLabel] = useState(null);
-    const [activeDiv, setActiveDiv] = useState(null);
-    const [showSuccessBanner, setShowSuccessBanner] = useState(false);
+export default function FeedItem({ rating, idx }) {
+  const [showFormDrawer, setShowFormDrawer] = useState(true);
+  const [formDrawerClosing, setFormDrawerClosing] = useState(false);
+  const chartRef = useRef(null);
+  const [formDrawerVisible, setFormDrawerVisible] = useState(false);
+  const [clickedLabel, setClickedLabel] = useState(null);
+  const [activeDiv, setActiveDiv] = useState(null);
+  const [showSuccessBanner, setShowSuccessBanner] = useState(false);
 
   const handleSuccess = () => {
     setShowSuccessBanner(true);
@@ -68,7 +83,6 @@ function FormDrawer({ onClose, visible, closing, rating, clickedLabel, onSuccess
       setShowSuccessBanner(false);
     }, 4000);
   };
-
 
   const toggleFormDrawer = (clickedLabelText) => {
     if (clickedLabelText !== activeDiv) {
@@ -81,73 +95,125 @@ function FormDrawer({ onClose, visible, closing, rating, clickedLabel, onSuccess
       setClickedLabel(null);
     }
   };
-    
-    const handleOutsideClick = (e) => {
-      if (formDrawerVisible && !e.target.closest('.form-drawer') && !e.target.closest('.highlight') && !e.target.closest('.lowlight')) {
-        setFormDrawerVisible(false);
-        setActiveDiv(null);
-      }
-    };
-    
+
+  const handleOutsideClick = (e) => {
+    if (
+      formDrawerVisible &&
+      !e.target.closest(".form-drawer") &&
+      !e.target.closest(".highlight") &&
+      !e.target.closest(".lowlight")
+    ) {
+      setFormDrawerVisible(false);
+      setActiveDiv(null);
+    }
+  };
+
   useEffect(() => {
-    document.addEventListener('mousedown', handleOutsideClick);
-    return () => document.removeEventListener('mousedown', handleOutsideClick);
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => document.removeEventListener("mousedown", handleOutsideClick);
   }, [formDrawerVisible]);
 
   const backgroundColors = [
-    'rgba(255, 99, 32, 0.8)', 'rgba(75, 192, 192, 0.8)', 'rgba(255, 206, 86, 0.8)', 'rgba(153, 102, 255, 0.8)',
-    'rgba(54, 162, 235, 0.8)', 'rgba(201, 203, 07, 0.8)', 'rgba(255, 159, 64, 0.8)', 'rgba(255, 99, 132, 0.8)',
+    "rgba(255, 99, 32, 0.8)",
+    "rgba(75, 192, 192, 0.8)",
+    "rgba(255, 206, 86, 0.8)",
+    "rgba(153, 102, 255, 0.8)",
+    "rgba(54, 162, 235, 0.8)",
+    "rgba(201, 203, 07, 0.8)",
+    "rgba(255, 159, 64, 0.8)",
+    "rgba(255, 99, 132, 0.8)",
   ];
 
   useEffect(() => {
     if (chartRef.current) chartRef.current.destroy();
     const chartCanvas = document.getElementById(`chart-${idx}`);
-    const scores = [rating.physiological, rating.safety, rating.love, rating.esteem, rating.knowledge, rating.aesthetics, rating.actualization, rating.transcendance];
+    const scores = [
+      rating.physiological,
+      rating.safety,
+      rating.love,
+      rating.esteem,
+      rating.knowledge,
+      rating.aesthetics,
+      rating.actualization,
+      rating.transcendance,
+    ];
     const chartColors = backgroundColors.slice(0, scores.length);
 
     const chartConfig = {
-      type: 'doughnut',
+      type: "doughnut",
       data: {
-        labels: ["Physiology", "Safety", "Love", "Esteem", "Cognition", "Aesthetics", "Actualization", "Transcendence"],
-        datasets: [{ label: '', data: scores, fill: true, backgroundColor: chartColors, borderColor: chartColors }],
+        labels: [
+          "Physiology",
+          "Safety",
+          "Love",
+          "Esteem",
+          "Cognition",
+          "Aesthetics",
+          "Actualization",
+          "Transcendence",
+        ],
+        datasets: [
+          {
+            label: "",
+            data: scores,
+            fill: true,
+            backgroundColor: chartColors,
+            borderColor: chartColors,
+          },
+        ],
       },
       options: {
         onHover: (event, chartElement) => {
           if (chartElement[0]) {
-            chartCanvas.style.cursor = 'pointer';
+            chartCanvas.style.cursor = "pointer";
           } else {
-            chartCanvas.style.cursor = 'default';
+            chartCanvas.style.cursor = "default";
           }
         },
         onClick: (event, elements) => {
           if (elements.length > 0) {
             const clickedElementIndex = elements[0].index;
-            const clickedLabelText = chartConfig.data.labels[clickedElementIndex];
+            const clickedLabelText =
+              chartConfig.data.labels[clickedElementIndex];
             toggleFormDrawer(clickedLabelText);
           }
         },
-        cutout: '55%',
+        cutout: "55%",
         plugins: {
           legend: { display: false },
           tooltip: {
-            callbacks: { label: (context) => `${chartConfig.data.labels[context.dataIndex]}: ${chartConfig.data.datasets[0].data[context.dataIndex]}` },
-            position: 'nearest', backgroundColor: 'rgba(0, 0, 0, 0.8)', titleFont: { size: 14 }, bodyFont: { size: 14 }, padding: 10, zIndex: 10000,
+            callbacks: {
+              label: (context) =>
+                `${chartConfig.data.labels[context.dataIndex]}: ${
+                  chartConfig.data.datasets[0].data[context.dataIndex]
+                }`,
+            },
+            position: "nearest",
+            backgroundColor: "rgba(0, 0, 0, 0.8)",
+            titleFont: { size: 14 },
+            bodyFont: { size: 14 },
+            padding: 10,
+            zIndex: 10000,
           },
-          hover: { mode: 'nearest', intersect: true },
+          hover: { mode: "nearest", intersect: true },
         },
       },
     };
     chartRef.current = new Chart(chartCanvas, chartConfig);
-  }, [rating,chartRef]);
+  }, [rating, chartRef]);
 
   return (
     <div className="feed-item-container">
-
-    {showSuccessBanner && (
+      {showSuccessBanner && (
         <div className="success-banner">
-          <p>Your suggestion has been submitted successfully! </p> &nbsp; &nbsp; 
+          <p>Your suggestion has been submitted successfully! </p> &nbsp; &nbsp;
           <br></br>
-          <button className="close-success-banner" onClick={() => setShowSuccessBanner(false)}>×</button>
+          <button
+            className="close-success-banner"
+            onClick={() => setShowSuccessBanner(false)}
+          >
+            ×
+          </button>
           <div className="loading-bar-container">
             <div className="loading-bar"></div>
           </div>
@@ -156,24 +222,28 @@ function FormDrawer({ onClose, visible, closing, rating, clickedLabel, onSuccess
 
       {/* Feed item info section */}
       <div className="feed-item-info">
-  <h1>{rating.user.username}</h1>
-  <div className="canvas-wrapper">
-    <i id="profile-picture" className="fas fa-user-circle" />
-    <canvas className="chart" id={`chart-${idx}`} />
-  </div>
-</div>
+        <h1>{rating.user.username}</h1>
+        <div className="canvas-wrapper">
+          <i id="profile-picture" className="fas fa-user-circle" />
+          <canvas className="chart" id={`chart-${idx}`} />
+        </div>
+      </div>
 
       {/* Lights container section */}
-      <div className='lights-container'>
+      <div className="lights-container">
         {/* Highlight section */}
         <div
           className="highlight"
           // onClick={() => toggleFormDrawer('highlight')}
-        ><>
-          <h2> <span id="username"> {rating.user.username} 's </span> highlight today was: </h2>
-          <br></br>
-          <p>  {rating.highlights} </p>
-          
+        >
+          <>
+            <h2>
+              {" "}
+              <span id="username"> {rating.user.username} 's </span> highlight
+              today was:{" "}
+            </h2>
+            <br></br>
+            <p> {rating.highlights} </p>
           </>
         </div>
 
@@ -182,18 +252,33 @@ function FormDrawer({ onClose, visible, closing, rating, clickedLabel, onSuccess
           className="lowlight"
           // onClick={() => toggleFormDrawer('lowlight')}
         >
-          {
-            rating.lowlights
-              ? <><h3> <span id="username"> {rating.user.username} 's </span> lowlight today was:</h3>
+          {rating.lowlights ? (
+            <>
+              <h3>
+                {" "}
+                <span id="username"> {rating.user.username} 's </span> lowlight
+                today was:
+              </h3>
               <br></br>
-              {rating.lowlights}</>
-              : <h3><span id="username"> {rating.user.username} </span> had no lowlight today.</h3>
-          }
+              {rating.lowlights}
+            </>
+          ) : (
+            <h3>
+              <span id="username"> {rating.user.username} </span> had no
+              lowlight today.
+            </h3>
+          )}
         </div>
       </div>
 
       {/* Form Drawer section */}
-      <FormDrawer onClose={() => setFormDrawerVisible(false)} visible={formDrawerVisible} rating={rating} clickedLabel={clickedLabel} onSuccess={handleSuccess} />
+      <FormDrawer
+        onClose={() => setFormDrawerVisible(false)}
+        visible={formDrawerVisible}
+        rating={rating}
+        clickedLabel={clickedLabel}
+        onSuccess={handleSuccess}
+      />
     </div>
   );
 }
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -1,128 +1,124 @@
-import jwtFetch from './jwt';
-import { RECEIVE_PIN } from './pins';
+import jwtFetch from "./jwt";
+// import { RECEIVE_PIN } from "./pins";
 
 const RECEIVE_CURRENT_USER = "session/RECEIVE_CURRENT_USER";
 const RECEIVE_SESSION_ERRORS = "session/RECEIVE_SESSION_ERRORS";
 const CLEAR_SESSION_ERRORS = "session/CLEAR_SESSION_ERRORS";
-export const RECEIVE_SEARCH_RESULTS = 'users/RECEIVE_SEARCH_RESULTS';
+export const RECEIVE_SEARCH_RESULTS = "users/RECEIVE_SEARCH_RESULTS";
 export const RECEIVE_USER_LOGOUT = "session/RECEIVE_USER_LOGOUT";
-export const ADD_USER_PIN = "users/ADD_USER_PIN"
+export const ADD_USER_PIN = "users/ADD_USER_PIN";
 
 // Dispatch receiveCurrentUser when a user logs in.
-const receiveCurrentUser = currentUser => ({
-  type: RECEIVE_CURRENT_USER,
-  currentUser
+const receiveCurrentUser = (currentUser) => ({
+	type: RECEIVE_CURRENT_USER,
+	currentUser,
 });
-  
+
 // Dispatch receiveErrors to show authentication errors on the frontend.
-const receiveErrors = errors => ({
-  type: RECEIVE_SESSION_ERRORS,
-  errors
+const receiveErrors = (errors) => ({
+	type: RECEIVE_SESSION_ERRORS,
+	errors,
 });
 
 // Dispatch logoutUser to clear the session user when a user logs out.
 const logoutUser = () => ({
-  type: RECEIVE_USER_LOGOUT
+	type: RECEIVE_USER_LOGOUT,
 });
 
 // Dispatch clearSessionErrors to clear any session errors.
 export const clearSessionErrors = () => ({
-  type: CLEAR_SESSION_ERRORS
+	type: CLEAR_SESSION_ERRORS,
 });
 
 export const receiveSearchResults = (results) => {
-  return {
-    type: RECEIVE_SEARCH_RESULTS,
-    results,
-  };
+	return {
+		type: RECEIVE_SEARCH_RESULTS,
+		results,
+	};
 };
 
 export const addUserPin = (suggestionId, userId) => {
-  return {
-    type: ADD_USER_PIN,
-    suggestionId,
-    userId,
-  };
+	return {
+		type: ADD_USER_PIN,
+		suggestionId,
+		userId,
+	};
 };
 
-export const signup = user => startSession(user, 'api/users/register');
-export const login = user => startSession(user, 'api/users/login');
+export const signup = (user) => startSession(user, "api/users/register");
+export const login = (user) => startSession(user, "api/users/login");
 
-export const getCurrentUser = () => async dispatch => {
-    const res = await jwtFetch('/api/users/current');
-    const user = await res.json();
-    return dispatch(receiveCurrentUser(user));
+export const getCurrentUser = () => async (dispatch) => {
+	const res = await jwtFetch("/api/users/current");
+	const user = await res.json();
+	return dispatch(receiveCurrentUser(user));
 };
 
-const startSession = (userInfo, route) => async dispatch => {
-    try {  
-      const res = await jwtFetch(route, {
-        method: "POST",
-        body: JSON.stringify(userInfo)
-      });
-      const { user, token } = await res.json();
-      localStorage.setItem('jwtToken', token);
-      return dispatch(receiveCurrentUser(user));
-    } catch(err) {
-      const res = await err.json();
-      if (res.statusCode === 400) {
-        return dispatch(receiveErrors(res.errors));
-      }
-    }
-  };
+const startSession = (userInfo, route) => async (dispatch) => {
+	try {
+		const res = await jwtFetch(route, {
+			method: "POST",
+			body: JSON.stringify(userInfo),
+		});
+		const { user, token } = await res.json();
+		localStorage.setItem("jwtToken", token);
+		return dispatch(receiveCurrentUser(user));
+	} catch (err) {
+		const res = await err.json();
+		if (res.statusCode === 400) {
+			return dispatch(receiveErrors(res.errors));
+		}
+	}
+};
 
-  export const logout = () => dispatch => {
-    localStorage.removeItem('jwtToken');
-    dispatch(logoutUser());
-  };
+export const logout = () => (dispatch) => {
+	localStorage.removeItem("jwtToken");
+	dispatch(logoutUser());
+};
 
-  const initialState = {
-    user: undefined
-  };
-  
+const initialState = {
+	user: undefined,
+};
+
 export const searchUsers = (searchTerm) => async (dispatch) => {
+	const response = await fetch(`/api/users/search?q=${searchTerm}`);
 
-    const response = await fetch(`/api/users/search?q=${searchTerm}`);
-
-    if (response.ok) {
-      const searchResults = await response.json();
-      dispatch(receiveSearchResults(searchResults));
-    }
-
-  
+	if (response.ok) {
+		const searchResults = await response.json();
+		dispatch(receiveSearchResults(searchResults));
+	}
 };
-  const sessionReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case RECEIVE_CURRENT_USER:
-        return { user: action.currentUser };
-      case RECEIVE_USER_LOGOUT:
-        return initialState;
-      case ADD_USER_PIN:
-        state[action.userId].pins.push(action.suggestionId);
-        return state;
-      case RECEIVE_SEARCH_RESULTS:
-        return {...state, searchResults: action.results }
-      default:
-        return state;
-    }
-  };
-  
-  const nullErrors = null;
 
-    export const sessionErrorsReducer = (state = nullErrors, action) => {
-    const newState = {...state}
-    switch(action.type) {
-        case RECEIVE_SESSION_ERRORS:
-          return action.errors;
-        case RECEIVE_CURRENT_USER:
-          
-        case CLEAR_SESSION_ERRORS:
-          return nullErrors;
-        default:
-          return state;
-    }
-    };
+const sessionReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case RECEIVE_CURRENT_USER:
+			return { user: action.currentUser };
+		case RECEIVE_USER_LOGOUT:
+			return initialState;
+		case ADD_USER_PIN:
+			state[action.userId].pins.push(action.suggestionId);
+			return state;
+		case RECEIVE_SEARCH_RESULTS:
+			return { ...state, searchResults: action.results };
+		default:
+			return state;
+	}
+};
 
-    
+const nullErrors = null;
 
-    export default sessionReducer;
+export const sessionErrorsReducer = (state = nullErrors, action) => {
+	// const newState = { ...state };
+	switch (action.type) {
+		case RECEIVE_SESSION_ERRORS:
+			return action.errors;
+		case RECEIVE_CURRENT_USER:
+			return nullErrors;
+		case CLEAR_SESSION_ERRORS:
+			return nullErrors;
+		default:
+			return state;
+	}
+};
+
+export default sessionReducer;
