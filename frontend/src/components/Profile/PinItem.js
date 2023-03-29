50273ade0e3fc5cba0bfdd7ad29f74f5ce3a602d
Madhur Luthra <71748091+mluthra01@users.noreply.github.com>
2023-03-28 23:54:46 -0700

    add like count and unpin to profile page

--- frontend/src/components/Profile/PinItem.js
+++ frontend/src/components/Profile/PinItem.js
@@ -1,59 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PinItem.css";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { createLike, deleteLike } from "../../store/likes";
 import { getPins, createPin, deletePin } from "../../store/pins";
 
-export default function PinItem({suggestion}) {
-    const dispatch = useDispatch();
-    const currentUser = useSelector(state => state.session.user);
-    const [isVisible, setIsVisible] = useState(true);
-    const pinIds = useSelector(getPins(currentUser._id));
+export default function PinItem({ suggestion }) {
+const dispatch = useDispatch();
+const currentUser = useSelector((state) => state.session.user);
+const [isVisible, setIsVisible] = useState(true);
+const pinIds = useSelector(getPins(currentUser._id));
+const [likeCount, setLikeCount] = useState(suggestion.likes.length);
+const [pinCount, setPinCount] = useState(suggestion.pins.length);
 
-    const likeDisplay = currentUser?.likes?.includes(suggestion._id) ? "Liked" : "Like";
-    const pinDisplay = "Pinned";
+const [likeDisplay, setLikeDisplay] = useState(
+currentUser?.likes?.includes(suggestion._id) ? "Liked" : "Like"
+);
+const pinDisplay = "Unpin";
 
-    const likeClick = () => {
-        dispatch(createLike(suggestion._id))
-    };
-    const unlikeClick = () => {
-    dispatch(deleteLike(suggestion._id))
-    };
+const likeClick = () => {
+dispatch(createLike(suggestion, currentUser._id));
+setLikeDisplay("Liked");
+setLikeCount(likeCount + 1);
+};
 
-    const pinClick = () => {
-    dispatch(createPin(suggestion._id, currentUser._id))
-    };
-    const unpinClick = () => {
-    dispatch(deletePin(suggestion._id ))
-    }
+const unlikeClick = () => {
+dispatch(deleteLike(suggestion._id));
+setLikeDisplay("Like");
+setLikeCount(likeCount - 1);
+};
 
-    return (
-        isVisible && (<div className="pin-item">
-            <div className="profile-part">
-                <i className="fas fa-user-circle fa-2x" />
-            </div>
-            <div className="suggestion-part">
-                <div className="category-tag">{suggestion.categoryTag}</div>
-                <h1>{suggestion.body}</h1>
-            </div>
-            <div className="buttons-part">
-                {/* <button>
-                    <i className="fas fa-heart" /> {likeDisplay}({suggestion.likes.length})
-                </button>
-                <button onClick={pinDisplay === "Pin" ? pinClick : unpinClick}>
-                    <i className="fas fa-thumbtack" /> {pinDisplay}({suggestion.pins.length})
-                </button> */}
-                            <button>
-                <i className="fas fa-heart" /> {likeDisplay}({suggestion.likes?.length || 0})
-            </button>
-            <button onClick={pinDisplay === "Pin" ? pinClick : unpinClick}>
-                <i className="fas fa-thumbtack" /> {pinDisplay}({suggestion.pins?.length || 0})
-            </button>
+const pinClick = () => {
+dispatch(createPin(suggestion, currentUser._id));
+};
 
-            </div>
-        </div>)
-    );
+const unpinClick = () => {
+dispatch(deletePin(suggestion._id));
+setIsVisible(false);
+};
+
+return (
+isVisible && (
+    <div className="pin-item">
+    <div className="profile-part">
+        <i className="fas fa-user-circle fa-2x" />
+    </div>
+    <div className="suggestion-part">
+        <div className="category-tag">{suggestion.categoryTag}</div>
+        <h1>{suggestion.body}</h1>
+    </div>
+    <div className="buttons-part">
+        <button onClick={likeDisplay === "Like" ? likeClick : unlikeClick}>
+        <i className="fas fa-heart" /> {likeDisplay}({likeCount})
+        </button>
+        <button onClick={pinDisplay === "Pin" ? pinClick : unpinClick}>
+        <i className="fas fa-thumbtack" /> {pinDisplay}
+        </button>
+    </div>
+    </div>
+)
+);
 }
-
